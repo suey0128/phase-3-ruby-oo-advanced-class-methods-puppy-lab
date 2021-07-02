18f4dd1d46f9ee4f3852e621a50aea471a1f07b0
suey0128 <syui0128@gmail.com>
2021-07-01 17:56:02 -0700

    commit tree bd2b8afb42f2d1b0454919cefb4ec6428f39d1b6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_reader :name
+    
+    def initialize name
+        @name = name
+        save
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.map{|dog| puts dog.name} 
+    end
+
+    def save
+        @@all << self
+    end
+end
